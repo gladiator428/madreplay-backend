956f20535aeb2ecc70ddfd5ac7ca621e52f12368
gladiator428 <h.j.dream0428@gmail.com>
2022-01-21 13:30:12 +0100

    publish email

---
+++ models/emails/Emails.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const EmailSchema = new mongoose.Schema({
+  e_id: {
+    type: String,
+    required: true,
+  },
+  publisher: {
+    type: String,
+    required: true,
+  },
+  from: {
+    type: String,
+  },
+  to: {
+    type: String,
+  },
+  date: {
+    type: Date,
+  },
+  snippet: {
+    type: String,
+  },
+  html: {
+    type: String,
+  },
+  publishDate: {
+    type: Date,
+  },
+});
+
+module.exports = mongoose.model("email", EmailSchema);
