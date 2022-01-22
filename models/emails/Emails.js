29ecefe8dc36b8cbffb0368499509b49ef01a366
gladiator428 <h.j.dream0428@gmail.com>
2022-01-22 05:37:41 +0100

    fqwe

--- models/emails/Emails.js
+++ models/emails/Emails.js
@@ -26,8 +26,8 @@ const EmailSchema = new mongoose.Schema({
   publishDate: {
     type: Date,
   },
-  likes: [{ email: { type: String } }],
-  unlikes: [{ email: { type: String } }],
+  likes: [String],
+  unlikes: [String],
 });
 
 module.exports = mongoose.model("email", EmailSchema);
