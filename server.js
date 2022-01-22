a07382e0f0b1eac11a940f8b74d7a8f86e86e126
gladiator428 <h.j.dream0428@gmail.com>
2022-01-22 07:52:12 +0100

    qwef

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ const { authMiddleware } = require("./middleware/auth");
 const connectDB = require("./config/db");
 const app = express();
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
+
 app.use(express.json({ extended: false }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
