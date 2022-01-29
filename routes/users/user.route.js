66d5c920600b5524b7dfbf96c43c7ca0349b439e
gladiator428 <h.j.dream0428@gmail.com>
2022-01-29 13:51:58 +0100

    reset password

--- routes/users/user.route.js
+++ routes/users/user.route.js
@@ -3,44 +3,58 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
 const fs = require("fs-extra");
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 const jwt = require("jsonwebtoken");
-const path = require("path");
+// const path = require("path");
 const smtpTransport = require("nodemailer-smtp-transport");
+const sendGridMail = require("@sendgrid/mail");
+const SENDGRID_API_KEY =
+  "SG.o_XiM7buQ_OnaRRlJz1BfAT0kPEXpIYj4RfdbbAqN8D16M21a95xJTwFtTd1p5yf4";
 
 const UserModel = require("../../models/users/Users");
 const getPath = require("../../utils/getPath");
 // const TOKEN_PATH = path.join(__dirname, "../../token.json");
-const sendEmailVerify = async (email, uniqueString) => {
-  const Transport = nodemailer.createTransport(
-    smtpTransport({
-      service: "gmail",
-      auth: {
-        user: "hotgold0905@gmail.com",
-        pass: "qhhmwouwjcgryjfj",
-      },
-    })
-  );
-  // GoldLion123:)
-  // qhhmwouwjcgryjfj
-  let mailOptions;
-  let sender = "hotgold0905@gmail.com";
-  mailOptions = {
-    from: sender,
+
+sendGridMail.setApiKey(SENDGRID_API_KEY);
+
+const sendEmailVerify = async (email, body) => {
+  return {
     to: email,
+    from: "verify@madreply.com",
     subject: "Verify your email",
-    html: `Press <a href=http://madreply.com/verify/${uniqueString}> here </a> to verify your email. Thank you.`,
+    text: "Press here to verify your email. Thank you.",
+    html: body,
   };
 
-  const temp = await Transport.sendMail(mailOptions)
-    .then((res) => {
-      return true;
-    })
-    .catch((e) => {
-      console.log(e);
-      return false;
-    });
-  return temp;
+  // const Transport = nodemailer.createTransport(
+  //   smtpTransport({
+  //     service: "gmail",
+  //     auth: {
+  //       user: "hotgold0905@gmail.com",
+  //       pass: "qhhmwouwjcgryjfj",
+  //     },
+  //   })
+  // );
+  // // GoldLion123:)
+  // // qhhmwouwjcgryjfj
+  // let mailOptions;
+  // let sender = "Madreply <madreply@gmail.com>";
+  // mailOptions = {
+  //   from: sender,
+  //   to: email,
+  //   subject: "Verify your email",
+  //   html: `Press <a href=http://madreply.com/verify/${uniqueString}> here </a> to verify your email. Thank you.`,
+  // };
+
+  // const temp = await Transport.sendMail(mailOptions)
+  //   .then((res) => {
+  //     return true;
+  //   })
+  //   .catch((e) => {
+  //     console.log(e);
+  //     return false;
+  //   });
+  // return temp;
 };
 
 // @route  GET /user
@@ -95,15 +109,21 @@ router.post("/register", async (req, res) => {
       await newUser.save();
       return res.json({ success: "You registered successful!" });
     } else {
-      const emailsent = await sendEmailVerify(newUser.email, uniqueString);
-      if (emailsent) {
-        await newUser.save();
+      // const emailsent = await sendEmailVerify(newUser.email, uniqueString);
+      // const emailsent =
+      try {
+        const body = `Press <a href=http://madreply.com/verify/${uniqueString}> here </a> to verify your email. Thank you.`;
+        await sendGridMail.send(sendEmailVerify(newUser.email, body));
         return res.json({
           success: "Please check your mailbox. Verify your email.",
         });
-      } else {
+      } catch (error) {
         return res.status(500).send("Email Send Error!");
       }
+      // if (emailsent) {
+      //   await newUser.save();
+      // } else {
+      // }
     }
   } catch (err) {
     console.log(error);
@@ -163,14 +183,19 @@ router.post("/resend/:email", async (req, res) => {
     { token: uniqueString }
   );
   try {
-    const emailsent = await sendEmailVerify(user.email, user.token);
-    if (emailsent) {
-      return res.json({
-        success: "Please check your mailbox. Verify your email.",
-      });
-    } else {
-      return res.status(500).json({ error: "Email Send Error!" });
-    }
+    // const emailsent = await sendEmailVerify(user.email, user.token);
+    // if (emailsent) {
+    //   return res.json({
+    //     success: "Please check your mailbox. Verify your email.",
+    //   });
+    // } else {
+    //   return res.status(500).json({ error: "Email Send Error!" });
+    // }
+    const body = `Press <a href=http://madreply.com/verify/${uniqueString}> here </a> to verify your email. Thank you.`;
+    await sendGridMail.send(sendEmailVerify(user.email, body));
+    return res.json({
+      success: "Please check your mailbox. Verify your email.",
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: "Email Send Error!" });
@@ -214,6 +239,42 @@ router.post("/logout", async (req, res) => {
     });
   }
   res.json({ success: "Logged out" });
+});
+
+router.get("/forget/:email", async (req, res) => {
+  const { email } = req.params;
+  const user = await UserModel.findOne({ email: email });
+  if (!user) {
+    return res
+      .status(404)
+      .json({ error: "You are not registered. Please Sign up now." });
+  }
+
+  try {
+    const body = `Press <a href=http://madreply.com/resetpass/${user.uniqueString}> here </a> to reset your password. Thank you.`;
+    await sendGridMail.send(sendEmailVerify(email, body));
+    return res.json({
+      success: "Please check your mailbox. Reset your Password.",
+    });
+  } catch (error) {
+    return res.status(500).json({ error: "Email Send Error!" });
+  }
+});
+
+router.post("/resetpass", async (req, res) => {
+  const { password, token } = req.body;
+  const salt = await bcrypt.genSalt(10);
+  const newPassword = await bcrypt.hash(password, salt);
+  try {
+    await UserModel.findOneAndUpdate(
+      { token: token },
+      { new: true },
+      { password: newPassword }
+    );
+    return res.json({ success: true });
+  } catch (error) {
+    return res.status(500).json({ error: "Server Error!" });
+  }
 });
 
 module.exports = router;
